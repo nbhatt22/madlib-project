d7113cebcae35fa5abd6a71a193f0e3e2c22d157
nbhatt22 <43207133+nbhatt22@users.noreply.github.com>
2018-10-09 08:30:21 -0400

    Add files via upload

---
+++ src/MadLib.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MadLib {
+
+	public static void main(String[] args) 
+	{
+		Scanner madlib = new Scanner(System.in)
+		System.out.print("Please enter your name: ");
+		String name = jawn.nextLine();
+		System.out.print("Please enter your favorite color: ");
+		String favColor = jawn.nextLine();
+		System.out.print("Please enter your favorite food: ");
+		String favFood = jawn.nextLine();
+		System.out.print("Please enter your favorite subject: ");
+		String favSubject = jawn.nextLine();
+		
+
+	}
+
+}
